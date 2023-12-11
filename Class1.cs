2a3818da9c2463eca72a0e9190ab2d612b68a348
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2023-12-11 08:12:38 +0000

    WebClient Scraping Class Library

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System.Net;
+
+namespace Scrape
+{
+    public class ScrapeWeb
+    {
+
+        public static string ScrapeWebpage(string address)
+        {
+            return GetHTML(address);
+        }
+
+        public static void ScarpeWebpage(string address, string filepath)
+        {
+            string html = GetHTML(address);
+            File.WriteAllText(filepath, html);
+        }
+
+        private static string GetHTML(string url)
+        {
+            string result;
+            WebClient client = new WebClient();
+            result = client.DownloadString(url);
+
+            return result;
+        }
+
+    }
+}
